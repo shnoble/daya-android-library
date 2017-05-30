6caadc05701476b1dba19f02965497be8a8603aa
shnoble <shnoble79@gmail.com>
2017-05-30 18:12:30 +0900

    Add a gradle script for uploading to 'jcenter'

--- daya-sample/src/main/java/com/daya/android/sample/MainActivity.java
+++ daya-sample/src/main/java/com/daya/android/sample/MainActivity.java
@@ -2,12 +2,18 @@ package com.daya.android.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.daya.android.utils.LocaleUtil;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, LocaleUtil.getCountry());
     }
 }
