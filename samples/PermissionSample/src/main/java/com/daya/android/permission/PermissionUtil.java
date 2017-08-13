3e7064ad1e37901b86569144e6d1a53ca1b9a44e
Shnoble <Shnoble@Shnobleui-MacBook-Pro.local>
2017-08-13 23:14:00 +0900

    Add comment to PermissionUtil methods

--- samples/PermissionSample/src/main/java/com/daya/android/permission/PermissionUtil.java
+++ samples/PermissionSample/src/main/java/com/daya/android/permission/PermissionUtil.java
@@ -13,16 +13,38 @@ import android.support.v4.content.ContextCompat;
 
 public class PermissionUtil {
 
+    /**
+     * Determine whether you have been granted a particular permission.
+     *
+     * @param permission The name of the permission being checked.
+     * @return true if you have the permission, or false if not.
+     */
     public static boolean hasPermission(@NonNull Context context,
                                         @NonNull String permission) {
         return (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED);
     }
 
+    /**
+     * Gets whether you should show UI with rationale for requesting a permission.
+     *
+     * @param activity The target activity.
+     * @param permission A permission your app wants to request.
+     * @return Whether you can show permission rationale UI.
+     */
     public static boolean shouldShowRequestPermissionRationale(@NonNull Activity activity,
                                                                @NonNull String permission) {
         return ActivityCompat.shouldShowRequestPermissionRationale(activity, permission);
     }
 
+    /**
+     * Requests permissions to be granted to this application.
+     *
+     * @param activity The target activity.
+     * @param permissions The requested permissions. Must me non-null and not empty.
+     * @param requestCode Application specific request code to match with a result
+     *    reported to {@link ActivityCompat.OnRequestPermissionsResultCallback#onRequestPermissionsResult(int, String[], int[])}
+     *    Should be >= 0.
+     */
     public static void requestPermissions(@NonNull Activity activity,
                                           @NonNull String[] permissions,
                                           int requestCode) {
