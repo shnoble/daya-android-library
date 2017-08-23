f47d3219b5af782ca014a202e19037429077f65a
shhong <shhong@shhongui-Mac-mini.local>
2017-08-23 19:40:43 +0900

    [FileSample] Add the external storage sample

---
+++ samples/FileSample/src/main/java/com/daya/android/file/sample/FileReader.java
@@ -0,0 +1,11 @@
+package com.daya.android.file.sample;
+
+import java.io.IOException;
+
+/**
+ * Created by shhong on 2017. 8. 21..
+ */
+
+public interface FileReader {
+    String read() throws IOException;
+}
--- samples/FileSample/src/main/java/com/daya/android/file/sample/MainActivity.java
+++ samples/FileSample/src/main/java/com/daya/android/file/sample/MainActivity.java
@@ -1,21 +1,30 @@
 package com.daya.android.file.sample;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresPermission;
 import android.support.annotation.WorkerThread;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int PERMISSION_REQUEST_CODE = 1000;
 
     private EditText mEditText;
 
@@ -25,7 +34,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mEditText = (EditText) findViewById(R.id.edit_text);
-        mEditText.setText("Android File IO Test");
+        mEditText.setText("Android Internal File IO Test");
+
+        String externalStorageInfo = "==================================================\n"
+                + "External Storage Information\n";
+
+        String externalStorageState = Environment.getExternalStorageState();
+        externalStorageInfo += "- External Storage State: " + externalStorageState + "\n";
+
+        if (externalStorageState.equals(Environment.MEDIA_MOUNTED)) {
+            String externalStorageDirectory = Environment.getExternalStorageDirectory().getAbsolutePath();
+            externalStorageInfo += "- External Storage Directory: " + externalStorageDirectory + "\n";
+
+            String rootDirectory = Environment.getRootDirectory().getAbsolutePath();
+            externalStorageInfo += "- Root Directory: " + rootDirectory + "\n";
+
+            String dataDirectory = Environment.getDataDirectory().getAbsolutePath();
+            externalStorageInfo += "- Data Directory: " + dataDirectory + "\n";
+
+            String DownloadCacheDirectory = Environment.getDownloadCacheDirectory().getAbsolutePath();
+            externalStorageInfo += "- Download Cache Directory: " + DownloadCacheDirectory + "\n";
+        }
+
+        Log.d(TAG, externalStorageInfo);
+
+        String permission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, permission + " permission granted.");
+
+            //chooseAccountIntent();
+        } else {
+            Log.w(TAG, permission + " permission denied.");
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                Log.d(TAG, "shouldShowRequestPermissionRationale: true");
+                ActivityCompat.requestPermissions(this, new String[]{permission}, PERMISSION_REQUEST_CODE);
+            } else {
+                Log.d(TAG, "shouldShowRequestPermissionRationale: false");
+                ActivityCompat.requestPermissions(this, new String[]{permission}, PERMISSION_REQUEST_CODE);
+            }
+        }
     }
 
     public void onClick(View view) {
@@ -40,6 +88,35 @@ public class MainActivity extends AppCompatActivity {
 
             case R.id.delete_button:
                 deleteFile();
+                break;
+
+            case R.id.save_external_button:
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                saveExternalFile();
+                break;
+
+            case R.id.load_external_button:
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                loadExternalFile();
                 break;
         }
     }
@@ -71,6 +148,77 @@ public class MainActivity extends AppCompatActivity {
         } else {
             mEditText.setText("Delete filed");
         }
+    }
+
+    @RequiresPermission(
+            allOf = {
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }
+    )
+    private void saveExternalFile() {
+        String externalStorageDirectory = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File directory = new File(externalStorageDirectory + "/dir");
+        if (!directory.mkdir()) {
+            Log.d(TAG, "Make directory failed: " + directory.getAbsolutePath());
+        }
+
+        File file = new File(externalStorageDirectory + "/dir/file.txt");
+
+        FileOutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(file);
+            String str = "This file exists in SDCard";
+            outputStream.write(str.getBytes());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    @RequiresPermission(
+            allOf = {
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            }
+    )
+    private void loadExternalFile() {
+        String externalStorageDirectory = Environment.getExternalStorageDirectory().getAbsolutePath();
+        FileInputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(externalStorageDirectory + "/dir/file.txt");
+            byte[] data = new byte[inputStream.available()];
+            while (inputStream.read(data) != -1) {}
+
+            Log.d(TAG, "Load Data: " + new String(data));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 
     private static class FileHandler {
@@ -163,6 +311,21 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public String toString() {
             return getMessage();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                Log.d(TAG, "Request Permissions Result:");
+                for (int i = 0; i < permissions.length; i++) {
+                    String result = (grantResults[i] == PackageManager.PERMISSION_GRANTED) ? "PERMISSION_GRANTED" : "PERMISSION_DENIED";
+                    Log.d(TAG, "- " + permissions[i] + ": " + result);
+                }
+                break;
         }
     }
 }
