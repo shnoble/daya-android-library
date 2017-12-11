00e965952824110aef7f1a90a3838fe6e623b02d
Shnoble <Shnoble@Shnobleui-MacBook-Pro.local>
2017-12-11 23:30:21 +0900

    Change to get the ad ID without dependency on the Google Play services library

--- daya-util/src/main/java/com/daya/android/info/AdvertisingInfo.java
+++ daya-util/src/main/java/com/daya/android/info/AdvertisingInfo.java
@@ -11,12 +11,8 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.WorkerThread;
 
+import com.daya.android.gms.AdvertisingIdClient;
 import com.daya.android.util.Validate;
-import com.google.android.gms.ads.identifier.AdvertisingIdClient;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-
-import java.io.IOException;
 
 /**
  * Created by Shnoble on 2017. 9. 22..
@@ -36,7 +32,7 @@ public final class AdvertisingInfo {
         return mAdvertisingId;
     }
 
-    public boolean ismIsLimitAdTrackingEnabled() {
+    public boolean isLimitAdTrackingEnabled() {
         return mIsLimitAdTrackingEnabled;
     }
 
@@ -56,25 +52,11 @@ public final class AdvertisingInfo {
         AdvertisingInfo advertisingInfo = null;
         try {
             AdvertisingIdClient.Info info = AdvertisingIdClient.getAdvertisingIdInfo(context);
-            if (info != null) {
-                advertisingInfo = new AdvertisingInfo(info.getId(), info.isLimitAdTrackingEnabled());
-            }
+            advertisingInfo = new AdvertisingInfo(info.getId(), info.isLimitAdTrackingEnabled());
 
-        } catch (IOException e) {
-            // Unrecoverable error connecting to Google Play services (e.g.,
-            // the old version of the service doesn't support getting AdvertisingId).
-            e.printStackTrace();
-        } catch (IllegalStateException e) {
-            // cannot be called on main thread
-            e.printStackTrace();
-        } catch (GooglePlayServicesNotAvailableException e) {
-            // Google Play services is not available entirely.
-            e.printStackTrace();
-        } catch (GooglePlayServicesRepairableException e) {
-            // Encountered a recoverable error connecting to Google Play services.
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
         return advertisingInfo;
     }
 }
