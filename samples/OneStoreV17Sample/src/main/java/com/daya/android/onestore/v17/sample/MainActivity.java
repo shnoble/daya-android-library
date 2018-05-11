727540a53d1b2cb58d44122797680f4a86398838
shnoble <shnoble79@gmail.com>
2018-05-11 14:34:26 +0900

    Create the `OneStoreV17Sample` app module

---
+++ samples/OneStoreV17Sample/src/main/java/com/daya/android/onestore/v17/sample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.daya.android.onestore.v17.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
